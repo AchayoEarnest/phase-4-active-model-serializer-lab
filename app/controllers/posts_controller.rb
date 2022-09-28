2f24299962f4ffe13fe027f72fee7f7f805e3b9e
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-09-29 02:31:59 +0300

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,22 @@ class PostsController < ApplicationController
 
   def index
     posts = Post.all
-    render json: posts
+    render json: posts, include: ['author', 'author.name']
   end
 
   def show
     post = Post.find(params[:id])
-    render json: post
+    render json: post, include: ['author', 'author.name']
+  end
+
+  def summary
+    post = Post.find(params[:id])
+    render json: post, serializer: PostSummarySerializer
+  end
+
+  def summaries
+    posts = Post.all
+    render json: posts, each_serializer: PostSummarySerializer
   end
 
   private
---
+++ app/serializers/post_summary_serializer.rb
@@ -0,0 +1,8 @@
+class PostSummarySerializer < ActiveModel::Serializer
+  attributes :summary
+
+
+  def summary
+    "#{self.object.title} - #{self.object.content[0..40]}..."
+  end
+end
