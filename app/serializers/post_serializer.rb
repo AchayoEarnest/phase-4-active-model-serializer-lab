0493a3a6f81117cced8b7fbfb5638bfac0a06693
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-01 04:39:55 +0300

    Done

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,10 +2,5 @@ class PostSerializer < ActiveModel::Serializer
   attributes :title, :content
 
   belongs_to :author
-  has_many :tags
-
-  # def content
-  #   "#{self.object.title} - #{self.object.content[0..40]}..."
-  # end
-
+  has_many :tags 
 end
